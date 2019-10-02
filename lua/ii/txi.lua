38761f7213841e00755e183d2464c85e5e60dc88
Greg Wuller <greg@afofo.com>
2019-10-01 21:32:06 -0700

    progress

--- lua/ii/txi.lua
+++ lua/ii/txi.lua
@@ -3,16 +3,21 @@ do return
 , manufacturer = 'bpc'
 , i2c_address  = 0x68
 , lua_name     = 'txi'
-, commands     = {}
+, commands     =
+  { { name = 'nop'
+    , cmd = 99
+    , args = { 'ignore', s8 }
+    }
+  }
 , getters =
   { { name = 'in'
-    , cmd  = 0
-    , args = { 'channel', s8 }
-    , retval = { 'value', u16 }
+    , cmd  = 0 + get_offset
+    , args = { 'channel', u8 }
+    , retval = { 'value', s16 }
     }
   , { name = 'param'
-    , cmd  = 2
-    , args = { 'channel', s8 }
+    , cmd  = 2 + get_offset
+    , args = { 'channel', u8 }
     , retval = { 'value', u16 }
     }
   }
